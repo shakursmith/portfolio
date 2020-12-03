e1463a98f06b30b190ce4c6f163397176636e780
Shakur Smith <57385924+shakursmith@users.noreply.github.com>
2020-12-02 21:16:32 -0600

    New Feature and Bug Fixes
    Added a typewriting effect. Fixed some styling errors.

--- script.js
+++ script.js
@@ -5,3 +5,40 @@ const navBar = document.getElementById('navBar');
 hamburgerBtn.addEventListener('click', () => {
     navBar.classList.toggle('open');
 });
+
+/* Cool New Typing Effect */
+const typeName = "Shakur Shabaazz";
+let i = 0;
+
+function typeText() {
+  if (i < typeName.length) {
+      document.getElementById("typing").innerHTML += typeName.charAt(i);
+      i++;
+      setTimeout(typeText, 300);
+  } if (document.getElementById("typing").innerHTML === typeName) {
+    $('#typing').addClass( "without-after-element" );
+  }
+}
+typeText();
+
+/* Smooth Scrolling Added for Extra Browser Support */
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+  
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 600, function(){
+  
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
+
+
+ 
